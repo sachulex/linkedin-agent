bcc58de6234486f3fef3cb3e3555039256c0867b
Uri Alexandrovitz <uri.alexandrovitz@gmail.com>
2025-09-01 23:38:29 +0300

    db: export pool and query

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,7 @@
 // src/db.ts
 import { Pool } from "pg";
 
-const pool = new Pool({
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.DATABASE_URL?.includes("neon.tech")
     ? { rejectUnauthorized: false }
